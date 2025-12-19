d72a9ee574d70cb6edec057fef312468dc691950
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2025-12-19 11:09:28 +0530

    sfdsfds

--- src/constants/TextConstant.js
+++ src/constants/TextConstant.js
@@ -1,3 +1,3 @@
-export const CURRENT_VERSION = "1.32";
+export const CURRENT_VERSION = "1.3";
 export const CACHE_PREFIX = "My_Resume_Cache_";
-export const RESUME_LINK = "https://portfolio-admin2644.netlify.app/";
+export const RESUME_LINK = "https://portfolio-admin2644.netlify.app/resume";
